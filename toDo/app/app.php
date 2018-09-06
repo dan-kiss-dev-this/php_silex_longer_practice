92c790e9e732ad1a703bd6c3be0039222bbdf5f6
Dan Kiss <flowfast47@gmail.com>
2018-09-06 14:39:00 -0700

    integrate twig into toDo

--- toDo/app/app.php
+++ toDo/app/app.php
@@ -13,47 +13,25 @@
     ));
 
     $app->get("/", function() use ($app) {
-        $output = '';
-        $all_tasks =Task::getAll();
 
-        if(!empty($all_tasks)) {
-          $output = $output . "
-          <h1>To Do List</h1>
-          <p>Here are all your tasks:</p>
-          ";
-        }
-
-        foreach ($all_tasks as $task) {
-            $output = $output . "<p>". $task->getDescription() . "</p>";
-        }
-
-        $output .= "
-            <form action='/tasks' method='post'>
-              <label for='description'>Task Description</label>
-              <input id='description' name='description' type='text'>
-
-              <button type='submit'>Add task</button>
-            </form>
-          ";
-
-          $output .= "
-            <form action='/delete_tasks' method='post'>
-              <button type='submit'>Delete</button>
-            </form>
-          ";
-
-        // return $output;
-        return $app['twig']->render('tasks.php', array('tasks' => Task::getAll()));
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->post("/tasks", function() {
-        $task = new Task($_POST['description']);
-        $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-        ";
+    // $app->post("/tasks", function() {
+    //     $task = new Task($_POST['description']);
+    //     $task->save();
+    //     return "
+    //         <h1>You created a task!</h1>
+    //         <p>" . $task->getDescription() . "</p>
+    //         <p><a href='/'>View your list of things to do.</a></p>
+    //     ";
+    // });
+
+    //mod
+    $app->post("/tasks", function() use ($app) {
+      $task = new Task($_POST['description']);
+      $task->save();
+      return $app['twig']->render('create_task.html.twig');
     });
 
     $app->post("/delete_tasks", function() {
